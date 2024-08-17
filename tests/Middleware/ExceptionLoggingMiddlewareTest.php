b9b288f0e988ff1a7acd309d3acda6a4939cfec8
Daniel Opitz <d.opitz@outlook.com>
2024-08-17 14:22:23 +0200

    Add ExceptionLoggingMiddleware tests

---
+++ tests/Middleware/ExceptionLoggingMiddlewareTest.php
@@ -0,0 +1,130 @@
+<?php
+
+/**
+ * Slim Framework (https://slimframework.com)
+ *
+ * @license https://github.com/slimphp/Slim/blob/5.x/LICENSE.md (MIT License)
+ */
+
+declare(strict_types=1);
+
+namespace Slim\Tests\Middleware;
+
+use ErrorException;
+use PHPUnit\Framework\TestCase;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestFactoryInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Log\LogLevel;
+use RuntimeException;
+use Slim\Builder\AppBuilder;
+use Slim\Middleware\EndpointMiddleware;
+use Slim\Middleware\ErrorHandlingMiddleware;
+use Slim\Middleware\ExceptionLoggingMiddleware;
+use Slim\Middleware\RoutingMiddleware;
+use Slim\Tests\Logging\TestLogger;
+
+class ExceptionLoggingMiddlewareTest extends TestCase
+{
+    public function testErrorExceptionIsLogged(): void
+    {
+        $this->expectException(ErrorException::class);
+
+        $app = (new AppBuilder())->build();
+
+        $logger = new TestLogger();
+        $app->add(new ExceptionLoggingMiddleware($logger));
+        $app->add(RoutingMiddleware::class);
+        $app->add(EndpointMiddleware::class);
+
+        // Set up a route that throws an ErrorException
+        $app->get('/error', function (ServerRequestInterface $request, ResponseInterface $response) {
+            throw new ErrorException('This is an error', 0, E_ERROR);
+            // trigger_error('This is an error', E_USER_ERROR);
+        });
+
+        $request = $app->getContainer()
+            ->get(ServerRequestFactoryInterface::class)
+            ->createServerRequest('GET', '/error');
+
+        try {
+            $app->handle($request);
+        } finally {
+            // Assert the logger captured the error
+            $logs = $logger->getLogs();
+            $this->assertCount(1, $logs);
+            $log = $logs[0];
+            $this->assertSame(LogLevel::ERROR, $log['level']);
+            $this->assertSame('This is an error', $log['message']);
+            $this->assertInstanceOf(ErrorException::class, $log['context']['exception']);
+        }
+    }
+
+    public function testThrowableIsLogged(): void
+    {
+        // Expect the RuntimeException to be thrown
+        $this->expectException(RuntimeException::class);
+
+        $app = (new AppBuilder())->build();
+
+        $logger = new TestLogger();
+        $app->add(new ExceptionLoggingMiddleware($logger));
+        $app->add(RoutingMiddleware::class);
+        $app->add(EndpointMiddleware::class);
+
+        // Set up a route that throws a generic Throwable
+        $app->get('/throwable', function (ServerRequestInterface $request, ResponseInterface $response) {
+            throw new RuntimeException('This is a runtime exception');
+        });
+
+        $request = $app->getContainer()
+            ->get(ServerRequestFactoryInterface::class)
+            ->createServerRequest('GET', '/throwable');
+
+        try {
+            $app->handle($request);
+        } finally {
+            // Assert the logger captured the error
+            $logs = $logger->getLogs();
+            $this->assertCount(1, $logs);
+            $log = $logs[0];
+            $this->assertSame(LogLevel::ERROR, $log['level']);
+            $this->assertSame('This is a runtime exception', $log['message']);
+            $this->assertInstanceOf(RuntimeException::class, $log['context']['exception']);
+        }
+    }
+
+    public function testUserLevelErrorIsLogged(): void
+    {
+        $this->expectException(ErrorException::class);
+
+        $app = (new AppBuilder())->build();
+
+        $logger = new TestLogger();
+        $app->add(ErrorHandlingMiddleware::class);
+        // $app->add(ExceptionHandlingMiddleware::class);
+        $app->add(new ExceptionLoggingMiddleware($logger));
+        $app->add(RoutingMiddleware::class);
+        $app->add(EndpointMiddleware::class);
+
+        $app->get('/error', function () {
+            trigger_error('This is an error', E_USER_ERROR);
+        });
+
+        $request = $app->getContainer()
+            ->get(ServerRequestFactoryInterface::class)
+            ->createServerRequest('GET', '/error');
+
+        try {
+            $app->handle($request);
+        } finally {
+            // Assert the logger captured the error
+            $logs = $logger->getLogs();
+            $this->assertCount(1, $logs);
+            $log = $logs[0];
+            $this->assertSame(LogLevel::ERROR, $log['level']);
+            $this->assertSame('This is an error', $log['message']);
+            $this->assertInstanceOf(ErrorException::class, $log['context']['exception']);
+        }
+    }
+}
