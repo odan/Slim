349aa6ba25e5c3c413e3b86e3a590039efa2d30f
Daniel Opitz <d.opitz@outlook.com>
2024-08-18 21:44:26 +0200

    Fix cs

--- Slim/Formatting/MediaTypeDetector.php
+++ Slim/Formatting/MediaTypeDetector.php
@@ -59,6 +59,6 @@ final class MediaTypeDetector
 
         $name = strtolower(trim($parts[0]));
 
-        return $name ? [$name => $name]: [];
+        return $name ? [$name => $name] : [];
     }
 }
