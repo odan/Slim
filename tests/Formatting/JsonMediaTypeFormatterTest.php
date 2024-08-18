6a05f7b2732762f1bc75640bdfcad09667b225f8
Daniel Opitz <d.opitz@outlook.com>
2024-08-18 21:14:20 +0200

    Add tests

--- tests/Formatting/JsonMediaTypeFormatterTest.php
+++ tests/Formatting/JsonMediaTypeFormatterTest.php
@@ -5,12 +5,12 @@ declare(strict_types=1);
 namespace Slim\Tests\Formatting;
 
 use Exception;
+use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\TestCase;
 use Psr\Http\Message\ResponseFactoryInterface;
 use Psr\Http\Message\ServerRequestFactoryInterface;
 use Slim\Builder\AppBuilder;
 use Slim\Formatting\JsonMediaTypeFormatter;
-use Slim\Renderers\JsonRenderer;
 
 class JsonMediaTypeFormatterTest extends TestCase
 {
@@ -32,7 +32,7 @@ class JsonMediaTypeFormatterTest extends TestCase
         $formatter = $app->getContainer()->get(JsonMediaTypeFormatter::class);
         $result = $formatter($request, $response, $exception, true);
 
-        $this->assertEquals('application/problem+json', $result->getHeaderLine('Content-Type'));
+        $this->assertEquals('application/json', $result->getHeaderLine('Content-Type'));
 
         $json = (string)$result->getBody();
         $data = json_decode($json, true);
@@ -63,7 +63,7 @@ class JsonMediaTypeFormatterTest extends TestCase
         $formatter = $app->getContainer()->get(JsonMediaTypeFormatter::class);
         $result = $formatter($request, $response, $exception, false);
 
-        $this->assertEquals('application/problem+json', $result->getHeaderLine('Content-Type'));
+        $this->assertEquals('application/json', $result->getHeaderLine('Content-Type'));
 
         $json = (string)$result->getBody();
         $data = json_decode($json, true);
@@ -73,5 +73,27 @@ class JsonMediaTypeFormatterTest extends TestCase
         $this->assertEquals('Application Error', $data['title']);
         $this->assertEquals(200, $data['status']);
         $this->assertArrayNotHasKey('exceptions', $data);
+    }
+
+    public function testSetContentType()
+    {
+        $app = (new AppBuilder())->build();
+
+        $request = $app->getContainer()
+            ->get(ServerRequestFactoryInterface::class)
+            ->createServerRequest('GET', '/');
+
+        $response = $app->getContainer()
+            ->get(ResponseFactoryInterface::class)
+            ->createResponse();
+
+        $exception = new Exception('Test exception message');
+
+        $formatter = $app->getContainer()->get(JsonMediaTypeFormatter::class);
+        $formatter->setContentType('application/vnd.api+json');
+
+        $result = $formatter($request, $response, $exception, false);
+
+        $this->assertEquals('application/vnd.api+json', $result->getHeaderLine('Content-Type'));
     }
 }
